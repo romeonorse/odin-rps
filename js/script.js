a0121cb350919aa7fd7d18a7634a468a35b3f52e
romeonorse <romeonorse@gmail.com>
2022-05-21 22:03:12 +0300

    Add GUI

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 // COMPUTER SELECTION
+let computerSelection;
+
 const computerPlay = () => {
 
   // Generate random number from 0-2
@@ -6,59 +8,82 @@ const computerPlay = () => {
   // Use switch to return value
   switch(randomNumber) {
     case 0:
-      return 'Rock';
+      computerSelection = 'rock';
       break;
     case 1:
-      return 'Paper';
+      computerSelection = 'paper';
       break;
     case 2:
-      return 'Scissors';
+      computerSelection = 'scissors';
       break;
   }
+  // console.log(computerSelection);
+  return computerSelection;
 }
 
-// PLAYER SELECTION
-const playerPrompt = () => {
-  // Take player's input and make it lowercase
-  let playerInput = prompt('Choose wisely between rock, paper, and scissors').toLowerCase();
+computerPlay();
 
-  if (playerInput === 'rock') {
-    return 'Rock';
-  } else if (playerInput === 'paper') {
-    return 'Paper';
-  } else if (playerInput === 'scissors') {
-    return 'Scissors'
-  } else {
-    console.log('Reload the page and try to choose correct answer');
-  }
+// PLAYER SELECTION
+// const playerPrompt = () => {
+  //   // Take player's input and make it lowercase
+  //   let playerInput = prompt('Choose wisely between rock, paper, and scissors').toLowerCase();
+  
+//   if (playerInput === 'rock') {
+//     return 'Rock';
+//   } else if (playerInput === 'paper') {
+//     return 'Paper';
+//   } else if (playerInput === 'scissors') {
+//     return 'Scissors'
+//   } else {
+//     console.log('Reload the page and try to choose correct answer');
+//   }
+// };
+
+let playerSelection;
+
+const images = document.querySelectorAll('img');
+const borderRemover = () => {
+  images[0].classList.remove('selected');
+  images[1].classList.remove('selected');
+  images[2].classList.remove('selected');
 };
 
-// PLAYING A GAME
-const playRound = (playerPrompt, computerPlay) => {
-  let result;
-  let computerSelection = computerPlay();
-  let playerSelection = playerPrompt();
+images.forEach(image => {
+  image.addEventListener('click', function(e) {
+    playerSelection = e.target.className;
+    borderRemover();
+    image.classList.add('selected');
+    // console.log(playerSelection, computerSelection);
+    // return playerSelection;
+  })
+})
 
-  if (playerSelection === 'Rock') {
-    if (computerSelection === 'Paper') {
+
+let result;
+
+// PLAYING A GAME
+const playRound = (playerSelection, computerSelection) => {
+
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'paper') {
       result = 'You lose! Paper beats Rock';
-    } else if (computerSelection === 'Scissors') {
-      result = 'You won! Rock beats scissors';
+    } else if (computerSelection === 'scissors') {
+      result = 'You won! Rock beats Scissors';
     } else {
       result = "It's a draw";
     }
-  } else if (playerSelection === 'Paper') {
-    if (computerSelection === 'Scissors') {
+  } else if (playerSelection === 'paper') {
+    if (computerSelection === 'scissors') {
       result = 'You lose! Scissors beats Paper';
-    } else if (computerSelection === 'Rock') {
+    } else if (computerSelection === 'rock') {
       result = 'You won! Paper beats Rock';
     } else {
       result = "It's a draw";
     }
-  } else if (playerSelection === 'Scissors') {
-    if (computerSelection === 'Rock') {
+  } else if (playerSelection === 'scissors') {
+    if (computerSelection === 'rock') {
       result = 'You lose! Rock beats Scissors';
-    } else if (computerSelection === 'Paper') {
+    } else if (computerSelection === 'paper') {
       result = 'You won! Scissors beats Paper';
     } else {
       result = "It's a draw";
@@ -69,23 +94,48 @@ const playRound = (playerPrompt, computerPlay) => {
   return result;
 };
 
-// playRound(playerPrompt, computerPlay);
 
-// PLAYING 5 ROUNDS
+
+// DISPLAYING RESULTS
+const button = document.querySelector('button');
+const message = document.getElementById('message');
+const endgame = document.getElementById('endgame');
+const inner = document.querySelector('.inner-container');
 
 let playerTotal = 0;
 let computerTotal = 0;
 
-for (let i = 0; i < 5; i++) {
-  let gameResult = playRound(playerPrompt, computerPlay);
+// PLAYING 5 ROUNDS
+button.addEventListener('click', function() {
+  computerPlay();
+  playRound(playerSelection, computerSelection);
+  message.textContent = result;
 
-  if (gameResult.includes('You won')) {
+  if (message.textContent.includes('You won')) {
     playerTotal++;
+    message.classList.add('won');
     console.log('One point to humans');
-  } else if (gameResult.includes('You lose')) {
-    compTotal++;
+  } else if (message.textContent.includes('You lose')) {
+    computerTotal++;
+    message.classList.add('lost');
     console.log('One point to machines');
+  } else {
+    message.classList.remove('won');
+    message.classList.remove('lost');
   }
-}
+  
+  endgame.textContent = `Player: ${playerTotal} - Computer: ${computerTotal}`;
 
-console.log(`Player: ${playerTotal} - Computer: ${computerTotal}`);
+  
+  if (playerTotal === 5) {
+    endgame.textContent = `Humanity won with 5 points against ${computerTotal}`;
+    inner.style.display = 'none';
+  } else if (computerTotal === 5) {
+    endgame.textContent = `Machines won with 5 points against ${playerTotal}`;
+    inner.style.display = 'none';
+  }
+});
+
+
+
+
